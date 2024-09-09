360091167bb040e0469883714f0543b54575b837
Igor <reisigorak@gmail.com>
2024-09-09 15:23:59 -0300

    displaying total

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const amount = document.querySelector("#amount");
 const currency = document.querySelector("#currency");
 const footer = document.querySelector("main footer");
 const description = document.querySelector("#description")
-
+const result = document.querySelector("#result")
 
 // Manipulando o input amount para receber somente números.
 amount.addEventListener("input", () => {
@@ -45,6 +45,19 @@ function convertCurrency(amount, price, symbol) {
     // Exibindo a cotação da moeda selecionada.
     description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}` 
 
+    //  Calcula o total
+    let total = amount * price;
+
+    // Verifica se o resultado não é um número.
+    if(isNaN(total)) {
+      return alert("Por favor, digite o valor corretamente para converter.")
+    }
+
+    // formatar o valor total.
+    total = formatCurrencyBRL(total).replace("R$", "")
+
+    // Exibe o resultado total.
+    result.textContent = `${total} Reais`;
 
     // Aplica a classe que exibe o footer para mostrar o resultado.
     footer.classList.add("show-result");
